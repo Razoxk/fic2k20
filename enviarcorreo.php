00e6ba1fe1d895ef6de62dbd445877a424a3cd4f
Pablo Orellana <porellana@tecnotec.cl>
2020-06-13 17:33:53 -0400

    enviar correo

---
+++ enviarcorreo.php
@@ -0,0 +1,26 @@
+<?php  
+
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "pablo.orellanaub@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje Postulante
+mail($correopostulante, $asunto, $carta);
+// Enviando Mensaje Representante
+mail($correopostulante, $asunto, $carta);
+// Enviando Mensaje a evaluador (alguien del gobierno)
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
+
+?>
